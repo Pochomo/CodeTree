42489a2374c01008d0439c61ca9ba87c6a9832aa
Pochomo <80620674+Pochomo@users.noreply.github.com>
2024-10-28 03:03:49 +0900

    Title: 함수를 이용한 369 게임, Time: 14ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241028/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 241028/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 bool AllDifferent(int n) {
-    return (n / 10) == 3 || (n % 10) == 3 || (n / 10) == 6 || (n % 10) == 6 || (n / 10) == 9 || (n % 10) == 9;
+    int x = 10;
+    while(x < 1000000){
+    while((n / x) == 3 || (n % 10) == 3 || (n / x) == 6 || (n % 10) == 6 || (n / x) == 9 || (n % 10) == 9){
+        return true;
+    }
+    x *= 10;
+    }
+    return false;
 }
 
 bool IsMagicNumber(int n) {
