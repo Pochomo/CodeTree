195e95c24aa7c17b18c377791c9939fd85369387
Pochomo <80620674+Pochomo@users.noreply.github.com>
2024-10-28 14:07:36 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241028/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -15,9 +15,14 @@ int main() {
     int num1, num2;
     int sum = 0;
     cin >> num1 >> num2;
+    if(num1 == num2){
+        cout << "0";
+    }
+    else{
     for(int i = num1; i <= num2; i++){
         sum += sosu(i);
     }
     cout << sum;
+    }
     return 0;
 }
