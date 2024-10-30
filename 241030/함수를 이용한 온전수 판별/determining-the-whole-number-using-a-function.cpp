aac3353b63ab441711bf2b8b7dba210944c737c1
Pochomo <80620674+Pochomo@users.noreply.github.com>
2024-10-30 18:16:38 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
+++ 241030/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-bool count_num(num1){
+bool count_num1(int num1){
     if(num1 % 2 == 0){
         return false;
     }
-    if(num1 % 10 = 5){
+    if(num1 % 10 == 5){
         return false;
     }
-    if(num1 % 3 == 0 && num1 % != 0){
+    if(num1 % 3 == 0 && num1 % 9 != 0){
         return false;
     }
     return true;
@@ -17,8 +17,9 @@ bool count_num(num1){
 int main() {
     int num1, num2;
     int sum = 0;
+    cin >> num1 >> num2;
     for(int i = num1; i <= num2; i++){
-        if(count_num(i)){
+        if(count_num1(i) == true){
             sum++;
         }
     }
