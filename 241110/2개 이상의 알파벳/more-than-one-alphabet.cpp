92e60b158964c6120fb3e4f460f2ba5162b3a057
Pochomo <80620674+Pochomo@users.noreply.github.com>
2024-11-10 15:45:03 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 241110/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -7,7 +7,7 @@ bool difference(const string& a){
     int n = a.length();
     int cnt = 1;
     for(int i = 0; i < n; i++){
-        if(a[0] != a[i]){
+        if(a[0] != a[i]){ //a[0] 부터 문자열 끝까지 비교해서 다르면 1증가
             cnt ++;
         }
     }
