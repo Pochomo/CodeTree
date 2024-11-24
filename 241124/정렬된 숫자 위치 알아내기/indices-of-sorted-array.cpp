52a017b1bb43c008e1f0c94d0f615d203ad040d3
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-24 20:16:10 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241124/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
+++ 241124/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -19,21 +19,32 @@ bool cmp(const Sorting & x1, const Sorting & x2){
 int main() {
     int n;
     int element;
+    int *tempnum;
     int *result;
     cin >> n;
     Sorting sortingArr[n];
+    tempnum = new int[n];
     result = new int[n];
     for(int i = 0; i < n; i++){
         cin >> element;
-        sortingArr[i] = Sorting(element, i);
+        sortingArr[i] = Sorting(element, i+1);
+    }
+
+    for(int i = 0; i < n; i++){
+        tempnum[i] = sortingArr[i].num;
     }
 
     sort(sortingArr, sortingArr + n, cmp);
 
+
     for(int i = 0; i < n; i++){
-        result[sortingArr[i].location] = i + 1;
-    }
-    
+        for(int j = 1; j <= n; j++){
+            if(tempnum[i] == sortingArr[j-1].num && i+1 == sortingArr[j-1].location){
+                result[i] = j;
+            }
+        }
+    }      
+
     for(int i = 0; i < n; i++){
         cout << result[i] << " ";
     }
