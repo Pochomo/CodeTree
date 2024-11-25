4617e9be6f1d72f58c69a22761d8c6b2a0f9f226
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-25 14:48:42 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/Date to Date/date-to-date.cpp
+++ 241125/Date to Date/date-to-date.cpp
@@ -7,16 +7,14 @@ int main() {
     int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int time = 0;
 
-    if(m1 == m2 && d1 == d2){
-        time++;
-    }
     while(true){
         if(m1 == m2 && d1 == d2){
+            time++;
             break;
         }
         time++;
         d1++;
-        if(d1 > num_of_days[m2]){
+        if(d1 > num_of_days[m1]){
             m1++;
             d1 = 1; 
         }
