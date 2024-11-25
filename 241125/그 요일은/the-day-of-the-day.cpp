3da9ee776a00633454d35afeedbd9f441ebf98c1
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-25 18:19:00 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/그 요일은/the-day-of-the-day.cpp
+++ 241125/그 요일은/the-day-of-the-day.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 
 int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-string day_of_week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
+//string day_of_week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+string day_of_week[] = {"Sun", "Sat", "Fri", "Thu", "Wed", "Tue", "Mon"};
 int calDate(int m1, int d1){
     int total_days = 0;
     for(int i = 1; i < m1; i++){
@@ -28,18 +28,13 @@ int main() {
     cin >> dateA;
 
     num = (calDate(m2, d2) - calDate(m1, d1));
-
     int temp = 0;
     for(int i = 0; i < 7; i++){
-        if(dateA == "Mon"){
-            temp = 7;
-            break;
-        }
         if(dateA == day_of_week[i]){
-            temp = i;
+            temp = i+1;
         }
     }
-    
+
     num += temp;
 
     cout << num / 7;
