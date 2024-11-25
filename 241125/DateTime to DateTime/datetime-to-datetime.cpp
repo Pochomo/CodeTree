1f65a449266c2ceec642afffd2f4d9c251916535
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-25 17:09:08 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/DateTime to DateTime/datetime-to-datetime.cpp
+++ 241125/DateTime to DateTime/datetime-to-datetime.cpp
@@ -7,7 +7,8 @@ void calTime(int a, int b, int c){
     day = 11;
     hour = 11;
     minute = 11;
-
+    //일 시간 분을 전부 분으로 환산해서 그 차이 계산하면 됨. (답지 코드)
+    //int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
     while(true){
         if(a < day){
             fin_minute = -1;
