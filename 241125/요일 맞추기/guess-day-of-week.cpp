0ccdc234fa9b10c807b7f404d187a77bd743a377
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-25 17:47:38 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241125/요일 맞추기/guess-day-of-week.cpp
+++ 241125/요일 맞추기/guess-day-of-week.cpp
@@ -50,3 +50,22 @@ int main() {
 
     return 0;
 }
+
+// 답지코드 날짜를 배열에 담아서 더 깔끔하게 만들었음.
+// int main() {
+//     // 변수 선언 및 입력
+//     int m1, m2, d1, d2;
+//     cin >> m1 >> d1 >> m2 >> d2;
+    
+//     // 두 날짜간의 차이가 몇 일인지를 구합니다.
+//     int diff = NumOfDays(m2, d2) - NumOfDays(m1, d1);
+    
+//     // 음수인 경우에는, 양수로 넘겨 계산해주면 올바르게 계산이 됩니다. 
+//     while(diff < 0)
+//         diff += 7;
+    
+//     // 알맞은 요일의 이름을 출력합니다.
+//     string day_of_week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+//     cout << day_of_week[diff % 7];
+//     return 0;
+// }
