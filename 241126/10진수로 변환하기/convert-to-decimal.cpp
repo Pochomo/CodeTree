28fa8bc462aa332097bc17c8abb09321bb1f33bc
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-26 14:02:25 +0900

    Title: 10진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/10진수로 변환하기/convert-to-decimal.cpp
+++ 241126/10진수로 변환하기/convert-to-decimal.cpp
@@ -19,3 +19,25 @@ int main() {
     cout << sum;
     return 0;
 }
+
+
+//int 변환 후 
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     // 변수 선언 및 입력
+//     string binary;
+//     cin >> binary;
+    
+//     // 십진수로 변환합니다.
+//     int num = 0;
+//     for(int i = 0; i < (int) binary.size(); i++)
+//         num = num * 2 + (binary[i] - '0'); //'0' 하는 이유는 char를 숫자로 변경할때
+//      '0;(1)을 뺴줘야함
+    
+//     // 출력
+//     cout << num;
+//     return 0;
+// }
