8d4d630e76ffd1c0fb72f87bdf6179eff8d7fddc
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-28 17:49:50 +0900

    Title: 왔다 갔던 구역 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241128/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 241128/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,56 +1,36 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-
 using namespace std;
 
-int inputN[100] = {0};
-char inputL[100];
-int result[2001] = {0};
-int MAXN = 2000;
+int list[2000] = {0};
 
 int main() {
-    int n;
-    char x;
-    int x1;
-    int OFFSET = 1000;
-
+    int n, x;
     cin >> n;
+    char dir;
+    int loc = 1000;
 
-    inputN[0] = 1000;
-
-    for(int i = 1; i <= n; i++){
-        cin >> x1 >> x;
-        inputN[i] = x1 + OFFSET;
-        inputL[i-1] = x;
-    }
-
-    int num = 1;
-    int current = OFFSET;
     for(int i = 0; i < n; i++){
-        if(inputL[num-1] == 'R'){
-            for(int i = OFFSET; i < inputN[num]; i++){
-                result[current]++;                
-                current++;     
-                }         
-            }             
-        if(inputL[num-1] == 'L'){             
-            for(int i = OFFSET; i < inputN[num]; i++){
-                current--;
-                result[current]++;                 
-            }         
-        }               
-        num++;     
-    }
-
-    int cnt = 0;
-    for(int i = 0; i < MAXN; i++){
-        if(result[i] > 1){
-            cnt++;
+        cin >> x >> dir;
+        if(dir == 'L'){
+            for(int j = loc; j >= loc - x; j--){
+                list[j]++;
+            }
+            loc -= (x+1);
+        }
+        else if(dir == 'R'){
+            for(int j = loc; j <= loc + x; j++){
+                list[j]++;
+            }
+            loc += (x-1);
         }
     }
 
+    int cnt=0;
+    for(int i = 0; i < 2000; i++){
+        if(list[i] > 1) {
+            cnt++;
+        }
+    }
     cout << cnt;
-
     return 0;
 }
