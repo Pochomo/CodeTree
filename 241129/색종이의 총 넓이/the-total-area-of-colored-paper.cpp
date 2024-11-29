4aaa8cb1dd3893f2aed1be50f495ea3c91c81f98
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-11-29 13:05:23 +0900

    Title: 색종이의 총 넓이, Time: 6ms, Memory: 1MB, Status: Passed - CodeTree

--- 241129/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
+++ 241129/색종이의 총 넓이/the-total-area-of-colored-paper.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int paperx = 8;
 int papery = 8;
-int result[200][200] = {0};
+int result[400][400] = {0};
 
 int main() {
     int n;
@@ -13,16 +13,16 @@ int main() {
     int x1, y1; //색종이 꼭지점의 위치
     for(int i = 0; i < n; i++){
         cin >> x1 >> y1; //색종이 꼭지점의 위치
-        for(int j = x1; j < x1 + paperx; j++){
-            for(int k = y1; k < y1 + papery; k++){
+        for(int j = x1 + OFFSET; j < x1 + paperx + OFFSET; j++){
+            for(int k = y1 + OFFSET; k < y1 + papery + OFFSET; k++){
                 result[j][k] = 1;
         }
     }
     }
 
     int cnt = 0;
-    for(int i = 0; i < 200; i++){
-        for(int j = 0; j < 200; j++){
+    for(int i = 0; i < 400; i++){
+        for(int j = 0; j < 400; j++){
             if(result[i][j] == 1){
                 cnt++;
             }
