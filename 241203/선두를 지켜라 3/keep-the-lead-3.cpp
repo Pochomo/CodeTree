4a68481cb15e74748b5658976d307b7af5217556
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-12-03 14:49:43 +0900

    Title: 선두를 지켜라 3, Time: 20ms, Memory: 2MB, Status: Runtime Error - CodeTree

---
+++ 241203/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int arrA[1000001] = {0};
+int arrB[1000001] = {0};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int v, t;
+    int index = 1;
+    int distance = 0;
+    for(int i = 0; i < n; i ++){
+        cin >> v >> t;
+        for(int j = 0; j < t; j++){
+            distance += v;
+            arrA[index] += distance;
+            index++;
+            
+        }
+    }
+    index = 1;
+    distance = 0;
+    for(int i = 0; i < n; i ++){
+        cin >> v >> t;
+        for(int j = 0; j < t; j++){
+            distance += v;
+            arrB[index] += distance;
+            index++; 
+        }
+    }
+    int cnt = 0;
+    //bool check = true;
+    for(int i = 0; i <= distance; i++){
+        if((arrA[i] != arrB[i] && arrA[i - 1] == arrB[i - 1])){
+            if(arrA[i - 1] == arrB[i - 1] && (i-1) != 0){
+                cnt = cnt + 1;
+            }
+            cnt ++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
