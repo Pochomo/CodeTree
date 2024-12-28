3d714ffc6cd5d8f0374c55cf2edbab8281110cac
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-12-29 01:07:20 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241229/체크판위에서 2/on-the-checkboard-2.cpp
+++ 241229/체크판위에서 2/on-the-checkboard-2.cpp
@@ -7,7 +7,6 @@ int main() {
     int r, c;
     cin >> r >> c;
     
-    // 격자 입력
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
             cin >> arrA[i][j];
@@ -18,19 +17,19 @@ int main() {
     char start_Point = arrA[0][0];
     char end_Point = arrA[r-1][c-1];
     
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            if (i > 0 && j > 0 && arrA[i][j] != start_Point) {
-                for (int k = i + 1; k < r; k++) {
-                    for (int l = j + 1; l < c; l++) {
-                        if (arrA[k][l] != arrA[i][j] && 
-                            arrA[k][l] != end_Point && 
-                            k < r-1 && l < c-1) {
-                            if (arrA[k][l] != arrA[i][j] && end_Point != arrA[k][l]) {
-                                result++;
-                            }
-                        }
-                    }
+    for (int i = 1; i < r; i++) {
+        for (int j = 1; j < c; j++) {
+            if(arrA[i][j] == start_Point){
+                continue;
+            }
+            for (int k = i + 1; k < r-1; k++) {
+                for (int l = j + 1; l < c-1; l++){
+                    
+                    if (arrA[k][l] == arrA[i][j]) continue;
+
+                    if (arrA[k][l] == end_Point) continue;
+                    
+                    result++;
                 }
             }
         }
