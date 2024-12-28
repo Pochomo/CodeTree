c3c5e61fd8a3e2d0fe1e6b3ece1c4a2ce44f5409
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2024-12-29 00:42:10 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241229/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+char arrA[15][15] = {'0'};
+
+
+
+int main() {
+    int r, c;
+    cin >> r >> c;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            cin >> arrA[i][j];
+        }
+    }
+
+    int cnt = 0;
+    int result = 0;
+    char start_Point = arrA[0][0];
+    char end_Point = arrA[r-1][c-1];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if(arrA[i][j] == start_Point){
+                continue;
+            }
+            for (int k = i + 1; k < r-1; k++) {
+                for (int l = j + 1; l < c-1; l++){
+                    
+                    if (arrA[k][l] == arrA[i][j]) continue;
+
+                    if (arrA[k][l] == end_Point) continue;
+                    
+                    result++;
+                }
+            }
+        }
+    }
+
+
+    cout << result;
+
+    return 0;
+}
