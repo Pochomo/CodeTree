02b72c1069b668f70f396344ec890c92fa4e6eaf
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-04 04:27:41 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250104/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int n;
+int numbers[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+    int ans = INT_MIN;
+    int temp = 0;
+    for (int i = 0; i < n; i++){
+        for(int j = i + 2; j < n; j++){
+            if(j < n){
+                temp = (numbers[i] + numbers[j]);
+            }
+        }
+        ans = max(temp, ans);
+        temp = 0;
+    }
+
+    cout << ans;
+    
+    return 0;
+}
