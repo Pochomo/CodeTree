e6eab102a2523111934c6d2c6210f937cb5954d6
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-04 04:34:59 +0900

    Title: 씨 오 더블유 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250104/씨 오 더블유 2/c-o-w-2.cpp
+++ 250104/씨 오 더블유 2/c-o-w-2.cpp
@@ -27,6 +27,14 @@ int main() {
         }
     }
 
+    // 답지코드
+    // for(int i = 0; i < n; i++)
+    //     for(int j = i + 1; j < n; j++)
+    //         for(int k = j + 1; k < n; k++)
+    //             if(str[i] == 'C' && str[j] == 'O' && str[k] == 'W')
+    //                 cnt++;
+    
+
     cout << cnt;
 
     return 0;
