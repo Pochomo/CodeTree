4102f95054b4f2950e366cb346567267ecc6104c
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-05 17:09:35 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250105/오목/O-mok.cpp
+++ 250105/오목/O-mok.cpp
@@ -1,120 +1,126 @@
 #include <iostream>
-
 using namespace std;
 
 int board[19][19];
 
 bool inrange(int i, int j){
-    return i < 19 && j < 19;
+    return i < 19 && j < 19 && i > 0 && j > 0;
 }
 
 int main() {
+    // 입력
     for (int i = 0; i < 19; i++){
         for (int j = 0; j < 19; j++){
             cin >> board[i][j];
         }
-    }   
-    //이긴 색 첫번째 줄에 출력, 
-    //연속된 5개의 바둑알 중 가운데 위치한 바둑알 번호 출력
-
-    int win = 0; //검 win 1 흰 win 2
+    }
+    
+    int win = 0; // 검 win 1 흰 win 2
     int middlei = 0, middlej = 0;
     int cnt = 0;
+    
     for (int i = 0; i < 19; i++){
         for (int j = 0; j < 19; j++){
+            // 검은 돌 체크
             if(board[i][j] == 1){
-                cnt = 0;
-                cnt++;
-                if(inrange(i, j + cnt) && board[i][j + cnt] == 1){
+                // 가로 방향 체크
+                cnt = 1;
+                while(inrange(i, j + cnt) && board[i][j + cnt] == 1){
                     cnt++;
-                    while(cnt != 5){
-                        if(inrange(i, j + cnt) && board[i][j + cnt] == 1){
-                            cnt++;
-                        }
-                        else{
-                            cnt = 0;
-                            break;
-                        }
-                    }
-                    if(cnt == 5){
-                        middlei = i;
-                        middlej = j + 2;
-                    }
-                }
-                else if(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 1){
-                    cnt++;
-                    while(cnt != 5){
-                        if(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 1){
-                            cnt++;
-                        }
-                        else{
-                            cnt = 0;
-                            break;
-                        }
-                    }
-                    if(cnt == 5){
-                        middlei = i + 2;
-                        middlej = j + 2;
-                    }
-                }
-                else{
-                    cnt = 0;
-                    continue;
                 }
                 if(cnt == 5){
+                    middlei = i;
+                    middlej = j + 2;
+                    win = 1;
+                }
+                
+                // 세로 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j) && board[i + cnt][j] == 1){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j;
+                    win = 1;
+                }
+                
+                // 대각선 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 1){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j + 2;
+                    win = 1;
+                }
+                
+                // 반대 대각선 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j - cnt) && board[i + cnt][j - cnt] == 1){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j - 2;
                     win = 1;
                 }
             }
-
+            
+            // 흰 돌 체크
             if(board[i][j] == 2){
-                cnt = 0;
-                cnt++;
-                if(inrange(i, j + cnt) && board[i][j + cnt] == 2){
+                // 가로 방향 체크
+                cnt = 1;
+                while(inrange(i, j + cnt) && board[i][j + cnt] == 2){
                     cnt++;
-                    while(cnt != 5){
-                        if(inrange(i, j + cnt) && board[i][j + cnt] == 2){
-                            cnt++;
-                        }
-                        else{
-                            cnt = 0;
-                            break;
-                        }
-                    }
-                    if(cnt == 5){
-                        middlei = i;
-                        middlej = j + 2;
-                    }
-                }
-                else if(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 2){
-                    cnt++;
-                    while(cnt != 5){
-                        if(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 2){
-                            cnt++;
-                        }
-                        else{
-                            cnt = 0;
-                            break;
-                        }
-                    }
-                    if(cnt == 5){
-                        middlei = i + 2;
-                        middlej = j + 2;
-                    }
-                }
-                else{
-                    cnt = 0;
-                    continue;
                 }
                 if(cnt == 5){
+                    middlei = i;
+                    middlej = j + 2;
+                    win = 2;
+                }
+                
+                // 세로 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j) && board[i + cnt][j] == 2){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j;
+                    win = 2;
+                }
+                
+                // 대각선 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j + cnt) && board[i + cnt][j + cnt] == 2){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j + 2;
+                    win = 2;
+                }
+                
+                // 반대 대각선 방향 체크
+                cnt = 1;
+                while(inrange(i + cnt, j - cnt) && board[i + cnt][j - cnt] == 2){
+                    cnt++;
+                }
+                if(cnt == 5){
+                    middlei = i + 2;
+                    middlej = j - 2;
                     win = 2;
                 }
             }
         }
-    }   
-
+    }
+    
     cout << win << endl;
     if(win != 0){
         cout << middlei + 1 << " " << middlej + 1;
     }
+    
     return 0;
 }
