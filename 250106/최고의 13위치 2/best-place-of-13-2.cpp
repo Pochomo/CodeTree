c2984b3bec792f4014eada165dc9b03b6df157c0
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-06 15:40:23 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250106/최고의 13위치 2/best-place-of-13-2.cpp
+++ 250106/최고의 13위치 2/best-place-of-13-2.cpp
@@ -6,37 +6,46 @@ using namespace std;
 int N;
 int arr[20][20];
 
-bool inrange(int i, int j, int z, int k) {
+bool inrange(int i, int j, int z, int k){
     return i < N && j < N && i >= 0 && j >= 0 && z < N && k < N && z >= 0 && k >= 0;
+}
+bool isoverlap(int i, int j, int z, int k){
+    for(int m = 0; m < 3; m++){
+        for(int n = 0; n < 3; n++){
+            if(i == z && j + m == k + n){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
 }
 
 int main() {
     cin >> N;
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
             cin >> arr[i][j];
         }
     }
     
-    // 겹치지 않는 1 * 3 크기 격자 2개 내에 들어올 수 있는 최대 동전의 수
-    int ans = INT_MIN;
+    //겹치지 않는 1 * 3 크기 격자 2개 내에 들어올 수 있는 최대 동전의 수
+    int ans = INT_MIN; 
     int num = 0;
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N-2; j++) {
-            for (int z = i; z < N; z++) {
-                for (int k = 0; k < N-2; k++) {
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N-2; j++){
+
+            for (int z = 0; z < N; z++){
+                for (int k = 0; k < N-2; k++){
                     num = 0;
-                    
-                    // 범위 벗어나면 continue
-                    if(!inrange(i, j, z, k)) {
+                    //범위 벗어나면 continue;
+                    if(!inrange(i, j, z, k)){
                         continue;
                     }
-                    
-                    // 안겹치면 더하기
-                    if(!(i == z && j == k)) {
-                        for(int m = 0; m < 3; m++) {
+                    //안겹치면 더하기
+                    if(!isoverlap(i, j, z, k)){
+                        for(int m = 0; m < 3; m++){
                             num += (arr[i][j+m]);
                             num += (arr[z][k+m]);
                         }
@@ -48,5 +57,6 @@ int main() {
     }
     
     cout << ans;
+
     return 0;
 }
