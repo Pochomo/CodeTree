cbedaf195f96ac9556b413ef27ebef59e80bc0e3
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-08 14:27:39 +0900

    Title: 특정 구간의 원소 평균값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 250108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -13,21 +13,22 @@ int main() {
     }
 
     int cnt = 0;
-    float avg; 
-    float num;
+    int avg; 
+    int num;
     for(int i = 0; i < n; i++){
-        avg = 0;
-        num = 0;
-        for(int j = i; j <= n; j++){
+        for(int j = i; j < n; j++){
             //구간 별로 평균값 구하기
+            num = 0;
             for(int k = i; k <= j; k++){
                 num += arr[k];
             }
-            avg = num / (abs(j - i + 1));
-            for(int z = 0; z < n; z++){
-                if(avg == (float)arr[z]){
-                    cnt++;
-                    break;
+            if((num % (j - i + 1)) == 0){
+                avg = (num / (j - i + 1));
+                for(int z = i; z <= j; z++){
+                    if(avg == arr[z]){
+                        cnt++;
+                        break;
+                    }
                 }
             }   
         }
