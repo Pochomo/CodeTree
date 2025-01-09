f014cd825320493ed218d2599c6668e2f79f883f
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-09 18:23:28 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250109/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 250109/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -17,31 +17,32 @@ int main() {
     //최소 T번 이상 H높이로 나오게끔 하고자함.
     //T개를 잡고 T개에서 모두 3 만들면 됨, 만드는데 얼마나 연산 많이했는지 count
     int ans = INT_MAX;
-    for(int size = T; size <= 10; size++) {
-        for(int i = 0; i <= N-size; i++){
-            int cost = 0;
-            int cnt = 0;
-            for(int j = i; j < i+size; j++){
-                int temp = arr[j];
-                if(temp == H){
-                    cnt++;
-                }  
-                else if(temp != H){
-                    while(temp != H){
-                        if(temp > H){
-                            cost++;
-                            temp--;
-                        }
-                        else if(temp < H){
-                            cost++;
-                            temp++;
-                        }
+    for(int i = 0; i <= N-T; i++){
+        int cost = 0;
+        int cnt = 0;
+        //int cal = 0;
+        for(int j = i; j < i+T; j++){
+            int temp = arr[j];
+            if(temp == H){
+                cnt++;
+            }  
+            else if(temp != H){
+                while(temp != H){
+                    if(temp > H){
+                        cost++;
+                        temp--;
                     }
-                    cnt++;
+                    else if(temp < H){
+                        cost++;
+                        temp++;
+                    }
                 }
+                cnt++;
             }
-            ans = min(ans , cost);
+
         }
+        //cout << cost;
+        ans = min(ans , cost);
     }
 
     cout << ans;
