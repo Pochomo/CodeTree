dd55a0f771a9094e13fc3b8cc8e49fc8e92829af
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-09 16:45:26 +0900

    Title: G or H 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250109/G or H 2/G-or-H-2.cpp
+++ 250109/G or H 2/G-or-H-2.cpp
@@ -18,14 +18,14 @@ int main() {
         GorH[pos[i]] = alpha[i];
         maxA = max(pos[i], maxA);
     }
-
+    
 
     //N명의 사람이 G or H 팻말을 들고있다.
     //오로지 G or H로만 혹은 G와 H가 같은 갯수로 나올 수 있도록 하는 최대 사진의 크기를 찾자
     //거리가 사진의 크기, 사람 1명이면 크기 0 -> 크기 = 사람 -1
     int ans = 0;
     for(int i = 0; i < maxA-1; i++){
-        for(int j = i+1; j < maxA; j++){
+        for(int j = i+1; j <= maxA; j++){
             int Gcnt = 0;
             int Hcnt = 0;
             int Sindex = 0;
@@ -48,7 +48,7 @@ int main() {
                 }
                 distance = (Eindex - Sindex);
             }
-            if((Gcnt == Hcnt) || (Gcnt == 0 || Hcnt == 0)){
+            if((Gcnt == Hcnt) || Gcnt == 0 || Hcnt == 0){
                 ans = max(ans, distance);
             }
         }
