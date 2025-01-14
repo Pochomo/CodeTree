0ab6010d6ec3983875bc6947ab068fa03e24d231
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-14 12:51:51 +0900

    Title: 삼각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250114/삼각형 만들기/create-triangle.cpp
+++ 250114/삼각형 만들기/create-triangle.cpp
@@ -3,28 +3,31 @@
 using namespace std;
 
 int N;
-int x[100];
-int y[100];
+long long x[100];
+long long y[100];
 
 int main() {
     cin >> N;
-
+    
     for (int i = 0; i < N; i++) {
         cin >> x[i] >> y[i];
     }
-    double ans = 0;
+    
+    long long ans = 0;
     for (int i = 0; i < N; i++) {
         for (int j = i+1; j < N; j++) {
             for (int k = j+1; k < N; k++){
-                double area = 0;
-                area = ((x[i]*y[j] + x[j]*y[k] + x[k]*y[i]) - (x[j]*y[i] + x[k]*y[j] + x[i]*y[k]));
-                area = abs(area);
-                ans = max(area, ans);
-            }            
+                if((x[i] == x[j] || x[j] == x[k] || x[k] == x[i])){
+                        if(y[i] == y[k] || y[j] == y[i] || y[k] == y[j]){
+                        long long area = abs((x[i]*y[j] + x[j]*y[k] + x[k]*y[i]) - 
+                                        (x[j]*y[i] + x[k]*y[j] + x[i]*y[k]));
+                        ans = max(area, ans);
+                    }
+                }
+            }
         }
     }
-
+    
     cout << ans;
-
     return 0;
 }
