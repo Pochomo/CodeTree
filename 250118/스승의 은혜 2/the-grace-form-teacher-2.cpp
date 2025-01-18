d68f2560a4017f8222292d0225a99f1e43f6fd8f
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-18 17:38:20 +0900

    Title: 스승의 은혜 2, Time: 51ms, Memory: 0MB, Status: Passed - CodeTree

--- 250118/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ 250118/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <algorithm> 
 
 using namespace std;
 
@@ -15,24 +16,31 @@ int main() {
         cin >> P[i];
     }
 
+    sort(P, P + N);
+
     int ans = INT_MIN;
     int cnt; //사줄 수 있는 학생 수
     //만약 예산을 넘어가면 
     for (int i = 0; i < N; i++) { //할인 받는 선물
-        int price1 = P[i] / 2;
+        int sales_price = P[i] / 2;
         for (int j = 0; j < N; j++) {
             cnt = 0;
             int budget = B;
             for (int k = j; k < N; k++) { //몇번부터 몇번까지 살 수 있는지 도는 loop
-                if(k == i){
-                    if(budget >= price1){
-                        cnt ++;
-                        budget -= price1;
+                if(budget > 0){
+                    if(k == i){ //sale된 가격을 만날 때
+                        if(budget >= sales_price){
+                            cnt ++;
+                            budget -= sales_price;
+                        }
                     }
-                }
-                else if(budget >= P[k]){
-                    cnt ++;
-                    budget -= P[k];
+                    else if(budget >= P[k]){
+                        cnt ++;
+                        budget -= P[k];
+                    }
+                    else{
+                        continue;
+                    }
                 }
                 else{
                     break;
