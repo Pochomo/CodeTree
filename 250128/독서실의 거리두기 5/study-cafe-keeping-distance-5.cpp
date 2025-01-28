2211812f0926d6d00929f7fc8f309938911166a8
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-28 16:53:33 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250128/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 250128/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -11,10 +11,11 @@ int main() {
     cin >> N;
     cin >> seat;
     //좌석의 개수 N개, 공석 여부가 주어지면 한 명의 인원 배치 후 최대한 거리두기를 실행한 간격을 출력하는 프로그램
-
     //1은 이미 차있는 곳 최소 1개의 1이 주어짐
     //거리는 0의 개수 + 1
-    int ans = INT_MIN;
+    int ans = INT_MAX;
+    int max_index = -1;//최대로 떨어지게 하는 index 위치
+    int max_distance = INT_MIN;
     for(int i = 0; i < N; i++){
         int distanceb = 0;
         int distance = 0;
@@ -43,7 +44,26 @@ int main() {
         else if(i == 0){
             distance = distancef;
         }
-        ans = max(distance, ans);
+        if(max_distance < distance){
+            max_distance = distance;
+            max_index = i;
+        }
+    }
+
+    seat[max_index] = '1';
+
+    //최소 거리 구하기
+    for(int j = 0; j < N; j++) {
+        if(seat[j] == '1') {
+            int temp = INT_MAX;
+            for(int k = j + 1; k < N; k++) {
+                if(seat[k] == '1') {
+                    temp = k - j;
+                    break;
+                }
+            }
+            ans = min(temp, ans);
+        }
     }
 
     cout << ans;
