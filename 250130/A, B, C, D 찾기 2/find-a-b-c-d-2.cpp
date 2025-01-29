7dfa414bd69810bd592c87c5dd333d4221545f11
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-30 00:18:20 +0900

    Title: A, B, C, D 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
+++ 250130/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
@@ -9,14 +9,11 @@ int nums[15];
 bool check(int a, int b, int c, int d) {
     int sums[15] = {a, b, c, d, a + b, b + c, c + d, d + a, a + c, b + d,
                     a + b + c, a + b + d, a + c + d, b + c + d, a + b + c + d};
-
+    sort(sums, sums + 15);
     int found = 0;
     for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 15; j++) {
-            if (nums[i] == sums[j]) {
-                found++;
-                break;
-            }
+        if (nums[i] == sums[i]) {
+            found++;
         }
     }
 
@@ -29,16 +26,19 @@ int main() {
     }
     sort(nums, nums + 15);
     int ans_a = nums[0]; //a는 고정됨
-    int ans_b = nums[1]; //b까지 고정됨
+    int ans_b = 0;
     int ans_c = 0;
     int ans_d = 0;
-    for (int c = ans_b; c <= 40; c++) { 
-        for (int d = c; d <= 40; d++) {
-            if (check(ans_a, ans_b, c, d)) { 
-                ans_c = c;
-                ans_d = d;
-                cout << ans_a << " " << ans_b << " " << ans_c << " " << ans_d;
-                return 0;
+    for (int b = nums[1]; b <= 40; b++) { 
+        for (int c = b; c <= 40; c++) { 
+            for (int d = c; d <= 40; d++) {
+                if (check(ans_a, b, c, d)) { 
+                    ans_b = b;
+                    ans_c = c;
+                    ans_d = d;
+                    cout << ans_a << " " << ans_b << " " << ans_c << " " << ans_d;
+                    return 0;
+                }
             }
         }
     }
