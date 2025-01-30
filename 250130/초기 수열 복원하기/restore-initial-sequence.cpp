2bbab7a16a296c4b6343a25cb2c32a16328f5d77
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-30 21:41:36 +0900

    Title: 초기 수열 복원하기, Time: 117ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 250130/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -6,6 +6,16 @@ int n;
 int adjacent[1001];
 int elements[1001];
 
+bool isDuplicate(int index) {
+    //중복 검사
+    for (int i = 1; i < index; i++) {
+        if (elements[i] == elements[index]) {
+            return true;
+        }
+    }
+    return false;
+}
+
 //1이상 N이하의 숫자들이 단 한 번씩만 등장하는 길이가 N인 수열 A
 //이 수열에서 인접한 원소의 합을 순서대로 구한 N-1개의 숫자가 주어졌을때, 초기 수열 A를 복원하는 프로그램
 int main() {
@@ -21,6 +31,9 @@ int main() {
         for (int i = 2; i < n + 1; i++) { //elements 정하는 loop
             for(int j = 1; j <= n; j++){
                 elements[i] = j;
+                if(isDuplicate(i)){
+                    continue;
+                }
                 if(elements[i] ==(adjacent[i - 1] - elements[i - 1])){
                     valid = true;
                     break; //맞는 값 찾음
