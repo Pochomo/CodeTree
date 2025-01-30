cb718ca9877b1ec7dfba928d96eb05941642e3c5
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-31 02:19:14 +0900

    Title: 언덕 깎기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250131/언덕 깎기/hill-cutting.cpp
+++ 250131/언덕 깎기/hill-cutting.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
-
 using namespace std;
 
 int N;
 int heights[1000];
+int cal[1000];
 
 int main() {
-
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> heights[i];
     }
-    //높이가 0부터 100, X만큼 올리거나 X만큼 깎을 때 가격은 X * X
-    //가장 높은 언덕, 가장 낮은 언덕 높이차 17이하 되도록하는 최소비용
-    sort(heights, heights + N);
-
+    
     int ans = INT_MAX;
-    for (int i = 0; i <= 100; i++) { //증가수치
+    for (int base = 0; base <= 100; base++) {
         int cost = 0;
-        for (int j = 0; j <= 100; j++) { //감소수치
-            int min_height = heights[0];
-            int max_height = heights[N-1];
-            int diff = abs(max_height - min_height);
-            max_height -= j;
-            min_height += i;
-            cost = (i * i) + (j * j);
-            if(abs(max_height - min_height) <= 17) ans = min(cost, ans);
-        }
         
+        for (int i = 0; i < N; i++) {
+            if (heights[i] < base) {
+                cost += (base - heights[i]) * (base - heights[i]);
+            }
+            else if (heights[i] > base + 17) {
+                cost += (heights[i] - (base + 17)) * (heights[i] - (base + 17));
+            }
+        }
+        ans = min(ans, cost);
     }
-
+    
     cout << ans;
-
-    return 0;   
+    return 0;
 }
