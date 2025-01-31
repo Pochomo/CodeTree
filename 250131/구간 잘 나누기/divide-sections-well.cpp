37a1d3c42476ee3687b83145c25d03ac6c4f8dd9
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-01-31 16:39:32 +0900

    Title: 구간 잘 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250131/구간 잘 나누기/divide-sections-well.cpp
+++ 250131/구간 잘 나누기/divide-sections-well.cpp
@@ -1,62 +1,44 @@
 #include <iostream>
-#include <climits>
+#include <algorithm>
+
+#define MAX_A 10000
+#define MAX_N 100
 
 using namespace std;
 
 int n, m;
-int a[201] = {};
-bool wall[201] = {};
-int ans = INT_MAX;
-
-int calculate() {
-    int max_num = INT_MIN;
-    int current_sum = 0;
-    
-    for (int i = 1; i < 2 * n + 1; i += 2) {
-        current_sum += a[i];
-        if (wall[i+1]) {
-            max_num = max(max_num, current_sum);
-            current_sum = 0;
-        }
-    }
-
-    return max_num;
-}
-
-void setwall(int start, int count){
-
-    if (count == m-1) {
-        int result = calculate();
-        ans = min(ans, result);
-        return;
-    }
-
-    for (int i = start; i < 2*n; i += 2) {
-        if (!wall[i]) {
-            wall[i] = true;
-            setwall(i + 2, count + 1);
-            wall[i] = false;
-        }
-    }
-
-}
+int a[MAX_N];
 
 int main() {
-    //숫자는 홀수 인덱스에 배치, 벽은 짝수 인덱스에 배치하는 방법
     cin >> n >> m;
-
-    //n개의 숫자가 주어지면 숫자 사이에 칸막이를 m-1개 둔다.
-    //m개의 구간으로 나눔 구간 합들 중 최댓값이 최소가 되게하는 프로그램
-    for (int i = 1; i < 2 * n + 1; i += 2) {
-        cin >> a[i];
-    }
-
-    wall[0] = true;
-    wall[2*n] = true;
     
-    //m-1개 두고 모든 경우의 수 따져서 계산
-    for (int i = 0; i < 2 * n; i += 2) {
-        setwall(i, 0);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    int ans = MAX_A;
+    for(int i = 1; i <= MAX_A; i++) {
+       
+        bool possible = true;
+        int section = 1;
+
+        int cnt = 0;
+        for(int j = 0; j < n; j++) {
+            if(a[j] > i) {
+                possible = false;
+                break;
+            }
+
+            if(cnt + a[j] > i) {
+                cnt = 0;
+                section++;
+            }
+
+            cnt += a[j];
+        }
+
+        if(possible && section <= m) {
+            ans = min(ans, i);
+        }
     }
 
     cout << ans;
