fedea7e8b22ad6d42828dc1e3c2669853022cf11
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-03 16:57:10 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250203/코딩톡/coding-talk.cpp
+++ 250203/코딩톡/coding-talk.cpp
@@ -25,23 +25,24 @@ int main() {
     // 두번째 줄부터, m개의 줄에 걸쳐 메세지 정보 c, u가 공백을 사이에 두고 주어짐.
     //   사람 c가 올린 메시지, 아직 메시지를 읽지 않은 사람 수가 u.
     // p의 숫자에 따라 몇 명인지 정해짐, u[i] == 0일 때를 빼곤 같거나 커야 함
-
-    for (int i = p - 1; i < m; i++){
-        read[c[i] - 'A'] = true;
-    }
-    
-    for (int i = p - 1; i < m; i++){
-        int j = i;
-        while(j > 0 && u[j] == u[j - 1]) {
-            read[c[j - 1] - 'A'] = true;
-            j--;
+    if(u[p - 1] == 0) {
+            return 0;
         }
+
+        for (int i = p - 1; i < m; i++){
+            read[c[i] - 'A'] = true;
+        }
+        
+        for (int i = p - 1; i > 0; i--){
+            if(u[i] == u[i - 1]){
+                read[c[i - 1] - 'A'] = true;
+            }
+        }
+        
+        for (int i = 0; i < n; i++){
+            if (!read[i])
+                cout << people[i] << " ";
+        }
+        
+        return 0;
     }
-    
-    for (int i = 0; i < n; i++){
-        if (!read[i])
-            cout << people[i] << " ";
-    }
-    
-    return 0;
-}
