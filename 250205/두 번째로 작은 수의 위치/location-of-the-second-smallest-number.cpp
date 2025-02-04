56db62d8d9e0eb5606d31912571eef3a6e98507e
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-05 01:55:43 +0900

    Title: 두 번째로 작은 수의 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250205/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int a[100];
+int b[100];
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+
+    sort(b, b + n);
+
+    //두번쨰로 작은원소 2개나 없으면 -1출력
+    int cnt = 0;
+    int first_element = b[1];
+    int second_element = 0;
+    for (int i = 1; i <= n; i++) {
+        if(b[1] != b[i]){ //무조건 달라야함
+            second_element = b[i];
+            break;
+        }
+    }
+
+    int index = 0;
+    for (int i = 1; i <= n; i++) {
+        if(a[i] == second_element){
+            index = i;
+            cnt++;
+        }
+    }
+
+    if(cnt == 1) cout << index;
+    else cout << -1;
+    return 0;
+}
