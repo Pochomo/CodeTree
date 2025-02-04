d6c7e7c917d0410733361713ceaf9aa1bbd56e7b
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-04 15:13:10 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250204/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 250204/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -10,6 +10,9 @@ int main() {
     //정렬돼있으면 0번
     //끝자리 하나만 연속되지않으면 1번
     //모두 연속되지 않으면 2번
+    //양쪽 끝에 서있는 사람 한 명을 선택해서 중앙에 놓는 거임
+
+    //무조건 답이 1이되는 경우 중앙이랑 한쪽이 2차이 날 때
     int cnt = 0;
     for(int i = 0; i < 2; i++){
         if(pos[i+1] - pos[i] == 2){
@@ -18,9 +21,20 @@ int main() {
             return 0; 
         }
     }
+
     cnt = 0;
     for(int i = 0; i < 2; i++){
-        if(pos[i+1] - pos[i] != 1) cnt++; 
+        if(pos[i+1] - pos[i] == 1) cnt++;
+        if(cnt == 2){
+            cout << 0;
+            return 0; 
+        }
+    }
+
+
+    cnt = 0;
+    for(int i = 0; i < 2; i++){
+        if(pos[i+1] - pos[i] != 2) cnt++; 
     }
 
     cout << cnt;
