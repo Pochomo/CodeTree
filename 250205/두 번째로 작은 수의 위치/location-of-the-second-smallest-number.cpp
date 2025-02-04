4d86f8ed227a9f6ce800f1cd37e9db279ddfe6a2
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-05 02:04:31 +0900

    Title: 두 번째로 작은 수의 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250205/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 250205/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -6,35 +6,46 @@ using namespace std;
 int n;
 int a[100];
 int b[100];
+
 int main() {
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    
+    for (int i = 0; i < n; i++) { // 인덱스 0부터 사용
         cin >> a[i];
         b[i] = a[i];
     }
 
-    sort(b, b + n + 1);
+    sort(b, b + n); 
 
-    //두번쨰로 작은원소 2개나 없으면 -1출력
-    int cnt = 0;
-    int first_element = b[1];
-    int second_element = 0;
-    for (int i = 1; i <= n; i++) {
-        if(b[1] != b[i]){ //무조건 달라야함
+    int first_element = b[0];
+    int second_element = -1;
+
+    for (int i = 1; i < n; i++) { 
+        if (b[i] != first_element) { 
             second_element = b[i];
             break;
         }
     }
 
-    int index = 0;
-    for (int i = 1; i <= n; i++) {
-        if(a[i] == second_element){
-            index = i;
+    if (second_element == -1) {
+        cout << -1;
+        return 0;
+    }
+
+    int index = -1;
+    int cnt = 0;
+
+    // 원래 배열에서 두 번째 최소값의 위치 찾기
+    for (int i = 0; i < n; i++) {
+        if (a[i] == second_element) {
+            index = i + 1; // 문제에서 1-based index 요구
             cnt++;
         }
     }
 
-    if(cnt == 1) cout << index;
+    // 두 번째 최소값이 정확히 1개일 경우 index 출력, 그렇지 않으면 -1 출력
+    if (cnt == 1) cout << index;
     else cout << -1;
+
     return 0;
 }
