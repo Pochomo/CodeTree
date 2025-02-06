98ef6df577652c6d202f741e077451c5693247b4
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-06 14:58:19 +0900

    Title: 세 수의 최대 곱, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250206/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int n;
+int arr[100000];
+int positive[100000];
+int negative[100000];
+int cnt_P = 0;
+int cnt_N = 0;
+bool zero = false;
+bool compare(int i, int j){
+    return j < i;
+}
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i] == 0) zero = true;
+        else if(arr[i] < 0) {
+            negative[i] = arr[i];
+            cnt_N++;
+        }
+        else if(arr[i] > 0){
+            positive[i] = arr[i];
+            cnt_P++;
+        } 
+    }
+
+    sort(negative, negative + n);
+    sort(positive, positive + n, compare);
+
+    //3개 최대 수 얻는 방법, 양수 최대 3개 곱하기 
+    //0 있는 경우, 없는 경우
+    int ans = INT_MIN;
+    int total_P = 1;
+    int total_N = 1;
+    if(zero){
+        ans = 0;
+    }
+
+    //음수 0개 양수 3개
+    if(cnt_P >= 3) {
+        for(int i = 0; i < 3; i++){
+            total_P *= positive[i];
+        }
+    }
+    ans = max(total_P, ans);
+    //음수 2개 양수 1개
+    if(cnt_N >= 2 && cnt_P >= 1) {
+        total_N *= positive[0];
+        for(int i = 0; i < 2; i++){
+            total_N *= negative[i];
+        }
+    }
+    ans = max(total_N, ans);
+
+    cout << ans;
+
+    return 0;
+}
