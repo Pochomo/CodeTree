12f9ae11db03405a7660a72b20b6fbfdf03a7d11
TaeGang Kim <80620674+Pochomo@users.noreply.github.com>
2025-02-08 16:28:35 +0900

    Title: 2개씩 그룹짓기 2, Time: 89ms, Memory: 1MB, Status: Passed - CodeTree

--- 250208/2개씩 그룹짓기 2/group-of-pairs-2.cpp
+++ 250208/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -16,12 +16,13 @@ int main() {
     sort(arr, arr + 2*n);
     //2개의 원소가 하나의 그룹을 이루도록 해 n 개 의 그룹, 2개의 수의 차이 중 최솟값이 최대가하도록
     //2 5 7 9 10 15
-    int ans = INT_MIN;
+    //int ans = INT_MIN;
+    int min_num = INT_MAX;
     for (int i = 0; i < n; i++) {
-        ans = max(ans, abs(arr[i] - arr[i + n - 1]));
+        min_num = min(min_num, abs(arr[i] - arr[i + n]));
     }
 
-    cout << ans;
+    cout << min_num;
 
     return 0;
 }
